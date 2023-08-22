8af1e545b2b22a47a13f64bbe114d727ff8dba61
Artur-Lepczynski <artur.lepczynski@gmail.com>
2023-08-22 14:55:58 +0200

    add: link option to the generic button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,27 +1,40 @@
+import { Link } from "react-router-dom";
 import { useTheme } from "../../hooks/useTheme";
 import style from "./Button.module.css";
 
 export default function Button(props) {
   //look: primary/secondary
+  //type: button/link
 
   const getClassName = useTheme(style);
-  
+
   let className;
-  if(props.disabled){
-    className = style["button-disabled"]
-  }else{
-    className = getClassName("button-"+props.look);
+  if (props.disabled) {
+    className = style["button-disabled"];
+  } else {
+    className = getClassName("button-" + props.look);
   }
-  
 
   return (
-    <button
-      className={`${style.button} ${className} ${props.className}`}
-      type="button"
-      disabled={props.disabled}
-      onClick={props.onClick}
-    >
-      {props.children}
-    </button>
+    <>
+      {props.type === "button" && (
+        <button
+          className={`${style.button} ${className} ${props.className}`}
+          type="button"
+          disabled={props.disabled}
+          onClick={props.onClick}
+        >
+          {props.children}
+        </button>
+      )}
+      {props.type === "link" && (
+        <Link
+          to={props.to}
+          className={`${style.button} ${style.link} ${className} ${props.className}`}
+        >
+          {props.children}
+        </Link>
+      )}
+    </>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,13 +6,16 @@ export default function Home(){
   return (
     <Page>
       <h1>Hello from Home.js</h1>
-      <Button look="primary">Button text</Button>
+      <Button type="button" look="primary">Button text</Button>
       <br></br>
       <hr></hr>
-      <Button look="secondary">Button text</Button>
+      <Button type="button" look="secondary">Button text</Button>
       <br></br>
       <hr></hr>
-      <Button look="secondary" disabled >Button text</Button>
+      <Button type="button" look="secondary" disabled >Button text</Button>
+      <br></br>
+      <hr></hr>
+      <Button type="link" look="primary">Button text</Button>
     </Page>
   )
 }
